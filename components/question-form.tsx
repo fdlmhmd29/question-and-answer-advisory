7b725d2797fc2aced3133bef3db5d6625ee54e01
Fadhil Muhammad <fadhilmhmd29@gmail.com>
2026-02-05 15:16:23 +0700

    Fix capitalization in question form label
    Change the Label text for the data_informasi field in components/question-form.tsx from "Data/Informasi Yang Diberikan" to "Data/Informasi yang Diberikan" to follow sentence-case capitalization and improve consistency.

--- components/question-form.tsx
+++ components/question-form.tsx
@@ -163,7 +163,7 @@ export function QuestionForm({
 
               <div className="flex flex-col gap-2">
                 <Label htmlFor="data_informasi">
-                  Data/Informasi Yang Diberikan
+                  Data/Informasi yang Diberikan
                 </Label>
                 <Textarea
                   id="data_informasi"
